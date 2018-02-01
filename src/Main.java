5f9c59ca957e921a124cced1ec32db4de38cc19d
Torres <agutorres16@gmail.com>
2018-02-01 14:59:04 -0500

    afficher + main

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,17 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		CV cv1 = new CV("Sarrazin", "William", "Collégiale", 1, XXXX, "Apprendre de nouvelles choses");
+		
+		CV cv2 = new CV();
+		
+		System.out.println("Bienvenue chez Barette!\n");
+		
+		cv1.afficher();
+		
+		cv2.afficher();
+		
+		
 	}
 
 }
