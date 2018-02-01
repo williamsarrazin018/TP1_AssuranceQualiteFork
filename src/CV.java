ac26396287d223c4c2879968d439e88429889e4a
Torres <1379547@cmontmorency.qc.ca>
2018-02-01 14:40:10 -0500

    Methode afficher()

--- src/CV.java
+++ src/CV.java
@@ -6,4 +6,22 @@ public class CV {
 	private int experienceTravail;
 	private String[] competences;
 	private String attentesCours;
+	
+	
+	public void afficher(){
+		System.out.println("Informations de\n"
+				+"Nom : "+this.nom+"\n"
+				+"Prénom : "+this.prenom+"\n"
+				+"Formation : "+this.formation +"\n"
+				+"Experience de travail : "+this.experienceTravail+"\n"
+				+"Compétences :");
+		
+		for (int i = 0; i < competences.length; i++) {
+			System.out.println(" "+this.competences[i] +" , ");
+		}
+		
+		System.out.println("\nAttentes du cours : "+this.attentesCours);
+				
+	}
 }
+
