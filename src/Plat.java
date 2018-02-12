338d56a795e96e87ce0b059ff8256a64691e4598
williamsarrazin018 <williamsarrazin@msn.com>
2018-02-12 08:40:17 -0500

    ClassePlatFinie

--- src/Plat.java
+++ src/Plat.java
@@ -1,4 +1,38 @@
 
 public class Plat {
-
+	private int prix;
+	private String nom;
+	
+	public Plat(int prix, String nom){
+		
+		this.nom = nom;
+		
+		this.prix = prix;
+		
+	}
+	
+	public void setNom(String nom){
+		
+		this.nom = nom;
+		
+	}
+	
+	public void setPrix(int prix){
+		
+		this.prix = prix;
+		
+	}
+	
+	public String getNom(){
+		
+		return this.nom;
+		
+	}
+	
+	public int getPrix(int prix){
+		
+		return this.prix;
+		
+	}
+	
 }
